e0194f6ccdc1da9c401048eee02989f844a00fb5
Yusuf Talha ARABACI <77548038+yusufarbc@users.noreply.github.com>
2025-10-04 19:21:47 +0300

    son son commit lets encrypt devredışı

--- elk_setup_ubuntu_jammy.sh
+++ elk_setup_ubuntu_jammy.sh
@@ -16,12 +16,12 @@ sudo apt-get update -y
 
 # Gerekli bağımlılıkların kurulması
 echo "[*] Gerekli bağımlılıklar kuruluyor..."
-sudo apt-get install -y apt-transport-https gnupg2 curl wget jq unzip lsb-release certbot python3-certbot-nginx
+sudo apt-get install -y apt-transport-https gnupg2 curl wget jq unzip lsb-release
 
 # Elastic GPG anahtarının eklenmesi
 echo "[*] Elastic GPG anahtarı ekleniyor..."
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/9.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-9.x.list
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
 # Paket listelerinin güncellenmesi
 echo "[*] Paket listesi tekrar güncelleniyor..."
@@ -38,6 +38,7 @@ sudo bash -c "echo 'discovery.type: single-node' >> /etc/elasticsearch/elasticse
 
 # Elasticsearch servisini başlatmak
 echo "[*] Elasticsearch servisi başlatılıyor..."
+sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch
 sudo systemctl start elasticsearch
 
@@ -98,44 +99,6 @@ echo "[*] Logstash servisi başlatılıyor..."
 sudo systemctl enable logstash
 sudo systemctl start logstash
 
-# Nginx kurulumu
-echo "[*] Nginx kuruluyor..."
-sudo apt-get install -y nginx
-
-# Nginx yapılandırması
-echo "[*] Nginx yapılandırması yapılıyor..."
-echo "
-server {
-    listen 80;
-    server_name <Sunucu_IP>;
-
-    location / {
-        proxy_pass http://localhost:5601;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-}
-" | sudo tee /etc/nginx/sites-available/kibana
-
-# Nginx sites-available'ı sites-enabled'a bağlama
-echo "[*] Nginx yapılandırması etkinleştiriliyor..."
-sudo ln -s /etc/nginx/sites-available/kibana /etc/nginx/sites-enabled/
-
-# Nginx yeniden başlatma
-echo "[*] Nginx servisi başlatılıyor..."
-sudo systemctl restart nginx
-
-# Let's Encrypt ile SSL sertifikası alalım
-echo "[*] Let's Encrypt ile SSL sertifikası alınıyor..."
-sudo certbot --nginx -d <Sunucu_IP> --non-interactive --agree-tos -m <email_adresiniz>
-
-# SSL ve HTTPs yapılandırmasını kontrol etme
-echo "[*] SSL sertifikası etkinleştirildi ve Nginx yeniden başlatılıyor..."
-sudo systemctl restart nginx
-
 # Kibana için Enrollment Token alınıyor
 echo "[*] Kibana için Enrollment Token alınıyor..."
 KIBANA_ENROLLMENT_TOKEN=$(sudo /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana)
@@ -143,6 +106,6 @@ echo "Kibana Enrollment Token: $KIBANA_ENROLLMENT_TOKEN"
 
 # Kibana erişimi bilgileri
 echo "[*] Kibana erişimi sağlandı. Aşağıdaki bilgileri kullanarak Kibana'ya erişebilirsiniz."
-echo "Kibana erişimi: https://<Sunucu_IP>:5601"
+echo "Kibana erişimi: https://$(hostname -I | awk '{print $1}'):5601"
 echo "Elastic kullanıcı adı: elastic, Parola: $ELASTIC_PASSWORD"
 echo "Kibana Enrollment Token: $KIBANA_ENROLLMENT_TOKEN"
